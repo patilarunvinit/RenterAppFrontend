5a0847efb5ef3caa40f11647f389a7b20e568c76
vinit <vinitpatil1001@gmail.com>
2024-07-19 15:13:05 +0530

    19 jul

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  
+  constructor() {}
+  
+  backphoto:string="assets/img/pexels-photo-2310713.jpeg"
+  centerlogo:string="assets/img/user-solid.svg"
+
+
+}
