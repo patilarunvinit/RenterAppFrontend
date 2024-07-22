634c1c6c11472728ce689af0d99a618557f0ad54
patilarunvinit <vinitpatil1001@gmail.com>
2024-07-23 01:42:58 +0530

    23-7-24

--- src/app/main-home/main-home.page.ts
+++ src/app/main-home/main-home.page.ts
@@ -19,5 +19,13 @@ export class MainHomePage implements OnInit {
   }
 
   backphoto:string="assets/img/pexels-photo-2310713.jpeg"
+  dp:string="assets/img/test1.jpg"
+  all_flats:string="assets/img/all_flats.png"
+  manage:string="assets/img/manage.png"
+  rent:string="assets/img/rent.png"
+  remain:string="assets/img/remain.png"
+  add_renter:string="assets/img/add_renter.png"
+  add_home:string="assets/img/add_home.png"
+
   
 }
